97903156369d3c421effa439ed442b64a41c43c2
swd762 <ilyakazalin@yandex.ru>
2020-03-10 13:02:07 +0400

    add about & advantages blocks with content

--- index.php
+++ index.php
@@ -5,10 +5,148 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>CrownWood</title>
+    <link rel="stylesheet" href="styles.css" type="text/css" media="all">
 </head>
 <body>
-<h1>hello world</h1>
+<header class="main-header">
+    <div class="container header">
+        <div class="header__logo">
+            <a href="#"><img src="img/logo.svg" alt="crownwood logo"></a>
+        </div>
+        <ul class="header__nav">
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">Продукция</a></li>
+            <li><a href="#">Где купить</a></li>
+        </ul>
+        <div class="header__tel">
+            <a href="tel:+8(888)888-88-88">+8 (888) 888-88-88</a>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="video-block__wrapper">
+        <div class="video-block__bg-container">
+            <video autoplay="autoplay" loop="loop" preload="auto" muted="muted">
+                <source src="img/video.mov" type="video/mp4">
+            </video>
+        </div>
+        <div class="video-block__bg-overlay">
+
+        </div>
+        <div class="container add-height">
+            <div class="video-block__title">
+                <h1>Деревянные полы</h1>
+                <h2>гармония для жизни</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="about-block__wrapper">
+        <div class="container">
+            <div class="about-block__content">
+                <section class="about_block__content-description">
+                    <h2>О нас</h2>
+                    <p>Мы&nbsp;занимаемся производством деревянных полов более 20&nbsp;лет. У&nbsp;нас сотни
+                        завершенных проектов высокой сложности и&nbsp;слаженая команда мастеров.<br><br>
+                    Мы&nbsp;знаем: Напольное покрытие&nbsp;&mdash; это основа уюта и&nbsp;комфорта в&nbsp;доме.</p>
+                    <p>Именно поэтому мы&nbsp;обращаем особое внимание на&nbsp;проработку деталей и&nbsp;точность
+                        технологических процессов, а&nbsp;значит можем гарантировать надежность и&nbsp;долговечность наших полов.</p>
+                </section>
+                <div class="about_block__content-thumbnails">
+                    <img src="img/about-1.jpg" alt="about thumbnails" style="width: 343px; height: 403px">
+                    <div class="imgs-container">
+                        <img src="img/about-2.jpg" alt="about thumbnails" style="width: 166px;height: 166px">
+                        <img src="img/about-3.jpg" alt="about thumbnails" style="width: 127px;height: 127px">
+                        <img src="img/about-4.jpg" alt="about thumbnails" style="width: 90px; height: 90px">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="advantages-block__wrapper">
+       <div class="container">
+        <div class="advantages-block__content">
+            <div class="advantages-block__content-col">
+                <section class="advantages-block__content-card">
+                    <img src="img/advantages-icon-1.svg" alt="advantages icon" style="margin-top: 9px">
+                    <h3>Гарантия - до 25 лет</h3>
+                    <p>Напольные покрытия Crownwood производятся на&nbsp;предприятии с&nbsp;20-летней историей.
+                        Производственная линия оснащена современным оборудованием немецкого, английского и
+                        итальянского производства — <strong>Braun Maschinen, DeVilBiss, SCM.</strong>
+                    </p>
+                    <p>
+                        Покрытие маслом осуществляется с&nbsp;помощью автоматизированной линии.
+                    </p>
+                    <p>
+                        Мы&nbsp;уверены в&nbsp;качестве нашего продукта и&nbsp;поэтому предоставляем гарантию до&nbsp;25&nbsp;лет.
+                    </p>
+                </section>
+                <section class="advantages-block__content-card">
+                    <img src="img/advantages-icon-3.svg" alt="advantages icon">
+                    <h3>Crownwood - это экологично</h3>
+                    <p>Мы&nbsp;покрываем паркет экологически чистыми немецкими маслами и&nbsp;лаками
+                        Biofa Naturprodukte W. Hahn BmbH, сертифицированными в&nbsp;соответствии с&nbsp;немецким стандартом DIN 53&nbsp;160.
+                    </p>
+                </section>
+                <section class="advantages-block__content-card">
+                    <img src="img/advantages-icon-5.svg" alt="advantages icon">
+                    <h3>Укладка паркета</h3>
+                    <p>В&nbsp;нашем штате профессиональные укладчики паркета с&nbsp;большим опытом,
+                        включающим сложные проекты и&nbsp;большие объёмы. Гарантируем высочайшее качество работ.
+                    </p>
+                </section>
+            </div>
+            <div class="advantages-block__content-col">
+                <section class="advantages-block__content-card">
+                    <img src="img/advantages-icon-2.svg" alt="advantages icon">
+                    <h3>Индивидуальный цвет паркета</h3>
+                    <p>
+                        Мы&nbsp;предоставляем нашим клиентам безграничные возможности для творчества.
+                    </p>
+                    <p>
+                        Любой из&nbsp;представленных вариантов паркета&nbsp;Вы можете заказать с&nbsp;индивидуальным подбором цвета.
+                    </p>
+                    <p>
+                        Единственное ограничение&nbsp;&mdash; площадь не&nbsp;менее 100м2.
+                    </p>
+                </section>
+                <section class="advantages-block__content-card card--margin">
+                    <img src="img/advantages-icon-4.svg" alt="advantages icon">
+                    <h3>Салоны в Москве и регионах</h3>
+                    <p>В&nbsp;любом из&nbsp;наших салонов можно посмотреть образцы напольных покрытий,
+                        виды расцветок, а&nbsp;также получить квалифицированную консультацию по&nbsp;выбору.
+                    </p>
+                </section>
+                <section class="advantages-block__content-card">
+                    <img src="img/advantages-icon-6.svg" alt="advantages icon">
+                    <h3>Цены</h3>
+                    <p>Паркет CrownWood имеет отличное соотношение цена-качество.
+                        При высоком качестве и услуг цены поддерживаются на&nbsp;приемлемом уровне.
+                        Это достигается за&nbsp;счёт тщательного отбора поставщиков сырья и&nbsp;тщательного контроля на&nbsp;всех этапах.
+                    </p>
+                </section>
+            </div>
+        </div>
+       </div>
+    </div>
+
+
+
+
+
+
+<h1>DDD</h1>
+
+
+
+
+
+</main>
+<footer>
+
+</footer>
 </body>
 </html>
 
