d2a1d44155e4566daa40a767a607a036367fe35b
swd762 <ilyakazalin@yandex.ru>
2020-03-18 13:59:13 +0400

    sticky menu

--- js/crownwood.js
+++ js/crownwood.js
@@ -82,15 +82,15 @@ for (let i = 0; i <no_dots.length ; i++) {
 // ***
 
 // Up button initialize
-$(document).ready(function() {
-    let defaults = {
-        containerID: 'toTop', // fading element id
-        containerHoverID: 'toTopHover', // fading element hover id
-        scrollSpeed: 1200,
-        easingType: 'linear'
-    };
-    $().UItoTop({ easingType: 'easeOutQuart' });
-});
+// $(document).ready(function() {
+//     let defaults = {
+//         containerID: 'toTop', // fading element id
+//         containerHoverID: 'toTopHover', // fading element hover id
+//         scrollSpeed: 1200,
+//         easingType: 'linear'
+//     };
+//     $().UItoTop({ easingType: 'easeOutQuart' });
+// });
 // ***
 
 // Mobile menu script
